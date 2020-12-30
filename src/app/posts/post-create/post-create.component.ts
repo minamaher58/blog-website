4c3cb454b8eb09c1e90bb6fa47ceb7b7b5a92224
minamaher58 <minamahertalaat58@gmail.com>
2020-12-30 10:37:50 +0200

    upload and edit image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { mimeType } from "./mime-type.validator";
 
 @Component({
   selector: 'app-post-create',
@@ -31,7 +32,7 @@ export class PostCreateComponent implements OnInit {
       'content': new FormControl(null, {
         validators: [Validators.required]
       }),
-      'image': new FormControl(null, {validators: [Validators.required]}),
+      'image': new FormControl(null, { validators: [Validators.required], asyncValidators: [mimeType] }),
     });
     this.route.paramMap.subscribe( (paramMap: ParamMap) => {
       if(paramMap.has('postId')) {
@@ -40,10 +41,11 @@ export class PostCreateComponent implements OnInit {
         this.isLoading = true;
         this.postService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
-          this.post = { id: postData._id, title: postData.title , content: postData.content};
+          this.post = { id: postData._id, title: postData.title , content: postData.content, imagePath: postData.imagePath};
           this.form.setValue({
             'title': this.post.title,
-            'content': this.post.content
+            'content': this.post.content,
+            'image': this.post.imagePath
           });
         });
       } else {
@@ -70,9 +72,9 @@ export class PostCreateComponent implements OnInit {
     }
     this.isLoading = true;
     if (this.mode === 'create') {
-      this.postService.addPost(this.form.value.title, this.form.value.content);
+      this.postService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     } else {
-      this.postService.updatePost(this.postId, this.form.value.title, this.form.value.content);
+      this.postService.updatePost(this.postId, this.form.value.title, this.form.value.content, this.form.value.image);
     }
     this.form.reset();
   }
